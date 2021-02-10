d73d332144ad506691be2b13833a51b3e168e78a
jasonweber99 <jasonweber99@gmail.com>
2021-02-09 17:23:17 -0800

    Created 'bare' homepage

--- TeamProject/MIVisitorCenter/Controllers/HomeController.cs
+++ TeamProject/MIVisitorCenter/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace MIVisitorCenter.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

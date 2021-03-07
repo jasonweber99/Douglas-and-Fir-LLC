d9206f5cb127072d6896c964efaafb2673bdc239
kelbyevans <63254837+kelbyevans@users.noreply.github.com>
2021-03-07 14:23:46 -0800

    fixed error with AddressId when updating business

--- TeamProject/MIVisitorCenter/Controllers/BusinessesController.cs
+++ TeamProject/MIVisitorCenter/Controllers/BusinessesController.cs
@@ -158,7 +158,7 @@ namespace MIVisitorCenter.Controllers
             {
                 return NotFound();
             }
-            ViewData["AddressId"] = new SelectList(_context.Addresses, "Id", "City", business.AddressId);
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "Id", "StreetAddress", business.AddressId);
             return View(business);
         }
 
@@ -168,7 +168,7 @@ namespace MIVisitorCenter.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = "admin")]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Phone,Website,PictureFileName,AddressId")] Business business)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Phone,Website,AddressId")] Business business)
         {
             if (id != business.Id)
             {
@@ -195,7 +195,7 @@ namespace MIVisitorCenter.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["AddressId"] = new SelectList(_context.Addresses, "Id", "City", business.AddressId);
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "Id", "StreetAddress", business.AddressId);
             return View(business);
         }
 

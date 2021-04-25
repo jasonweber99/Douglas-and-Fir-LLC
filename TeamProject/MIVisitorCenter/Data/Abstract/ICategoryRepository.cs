fec27b8256a8c60d2fd62ce14fd3324e3f5d1741
kelbyevans <63254837+kelbyevans@users.noreply.github.com>
2021-04-24 19:25:13 -0700

    created category repository and wrote all methods

---
+++ TeamProject/MIVisitorCenter/Data/Abstract/ICategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using MIVisitorCenter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIVisitorCenter.Data.Abstract
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        IQueryable<Category> GetAllLodging();
+
+        IQueryable<Category> GetBusinessesByCategory(string category);
+
+        IQueryable<Category> GetAllActivities();
+
+        IQueryable<Category> GetAllArtAndCulture();
+
+        Business GetRandomBusinessByCategory(string category);
+    }
+}
---
+++ TeamProject/MIVisitorCenter/Data/Concrete/CategoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using MIVisitorCenter.Data.Abstract;
+using MIVisitorCenter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIVisitorCenter.Data.Concrete
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(MIVisitorCenterDbContext ctx) : base(ctx)
+        {
+
+        }
+
+        public virtual IQueryable<Category> GetAllLodging()
+        {
+            return _dbSet.Where(c => c.Name == "Lodging").AsQueryable();
+        }
+
+        public virtual IQueryable<Category> GetBusinessesByCategory(string category)
+        {
+            return _dbSet.Where(c => c.Name == category).AsQueryable();
+        }
+
+        public virtual IQueryable<Category> GetAllActivities()
+        {
+            return _dbSet.Where(n => n.Name == "Hiking" ||
+                n.Name == "Cycling" ||
+                n.Name == "Birding" ||
+                n.Name == "Fishing" ||
+                n.Name == "Wineries" ||
+                n.Name == "Golf");
+        }
+
+        public virtual IQueryable<Category> GetAllArtAndCulture()
+        {
+            return _dbSet.Where(n => n.Name == "Historic Sites & Museums" ||
+                n.Name == "Art Galleries" ||
+                n.Name == "Cinemas & Performing Arts");
+        }
+
+        public virtual Business GetRandomBusinessByCategory(string category)
+        {
+            Random random = new Random();
+            var businesses = _dbSet.Where(c => c.Name == category);
+            int index = random.Next(businesses.ElementAt(0).BusinessCategories.Count());
+            return businesses.ElementAt(0).BusinessCategories.ElementAt(index).Business;
+        }
+    }
+}
